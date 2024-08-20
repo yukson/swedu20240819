11845f1ef2243d82160386a4c59e9a4e5b20ba25
yukson <youn9249@naver.com>
2024-08-20 11:06:07 +0900

    react todolist step01

--- day16/src/App.js
+++ day16/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import {useState} from "react";
 import ItemRow from "./ItemRow";
+import Input from "./input";
 
 const App = ()=>{
     // 전역변수를 state로 만들어 주어야 re rendering 된다.
@@ -14,23 +15,13 @@ const App = ()=>{
     ]);
     const [noCnt, setNoCnt] = useState(105);
 
-    const [inputTitle, setInputTtile] = useState("");
-    const [outputTitle, setOutputTtile] = useState("");
-
-
     const [flag, setFlag] = useState(false);
 
-    const onClickEvent = () => {
+    const onClickEvent = (inputTitle) => {
         // 기존 내용에 새 내용을 추가 해서 새 배열을 생성
         setTodoLilst([...todoList, {no:noCnt, title:inputTitle, done: false}]);
         setNoCnt(noCnt+1);
-        setInputTtile("");
     }
-
-    const onChangeTitle = (e) => {
-        setInputTtile(e.target.value);
-    }
-
     const onDelete = ({no, title, done}) => {
         const newList = todoList.filter((todo)=> {
             return todo.no != no;
@@ -66,16 +57,8 @@ const App = ()=>{
         <div className="App-header">
             <h1>{name} App</h1>
         </div>
-        <div className="input-title">
-          <div className="container" style={{padding: "10px"}}>
-            <div className="input-group mb-3">
-                <input value={inputTitle} onChange={onChangeTitle} type="text" className="form-control"/>
-                <div className="input-group-append">
-                    <button className="btn btn-success" onClick={onClickEvent}>Save</button>
-                </div>
-            </div>
-          </div>
-        </div>
+        {/* todo 타이틀 입력 컴포넌트 위치*/}
+        <Input onClickEvent={(onClickEvent)} />
         <div className="list-body">
           <div className="container">
           <table className="table table-hover">
---
+++ day16/src/input.js
@@ -0,0 +1,41 @@
+import { useRef, useState } from "react";
+
+const Input = ({onClickEvent}) => {
+
+    const [inputTitle, setInputTitle] = useState("");
+
+    // 훅은 콜백함수에 포함될 수 없다.
+    const inputFocus = useRef(null);
+
+    return (
+        <div className="input-title">
+          <div className="container" style={{padding: "10px"}}>
+            <div className="input-group mb-3">
+                <input autoFocus ref={inputFocus} value={inputTitle} onChange={(e)=> setInputTitle(e.target.value)} type="text" className="form-control"/>
+                <div className="input-group-append">
+                    <button className="btn btn-success" onClick={(e) =>{
+                        onClickEvent(inputTitle);
+                        setInputTitle("");
+                        // useRef() 훅 사용 focus를 잡아준다.
+                        inputFocus.current.focus();
+                    }}>Save</button>
+                </div>
+            </div>
+          </div>
+        </div>
+    );
+}
+
+// 대표 컴포넌트를 하나만 모듈 등록
+export default Input;
+
+//const InputSub = () => {
+//    return (<>
+    
+//    </>);
+//}
+
+// 여러 컴포넌트를 모듈로 등록할 때 
+//export {Input, InputSub};
+
+// 불러올 때 import {Input, InputSub} from "./Input"
